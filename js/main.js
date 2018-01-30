6a40c109050921c535ccb840edab380285aa7b96
Unknown <riderjensen1@gmail.com>
2018-01-30 11:45:28 -0700

    Tween Max no longer stutters when scrolling

--- js/main.js
+++ js/main.js
@@ -6,23 +6,11 @@ window.onscroll = function() {scrollFunction();};
 function scrollFunction() {
 	if(window.innerWidth >= 768){
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-		if  (!document.getElementById("navbar-top").className.match(/(?:^|\s)navbar-default(?!\S)/)) {
-			document.getElementById("navbar-top").className += " navbar-default";
 			TweenMax.to(".navbar-default", 0.5, {opacity: 1, delay: 0.1});
-		}
     }
      else {
-     	if  (document.getElementById("navbar-top").className.match(/(?:^|\s)navbar-default(?!\S)/)) {
 			TweenMax.to(".navbar-default", 0.5, {opacity: 0, delay: 0.1});
-			document.getElementById("navbar-top").className = document.getElementById("navbar-top").className.replace( /(?:^|\s)navbar-default(?!\S)/g , '' );
 			
 		}
-     	
-    	}
-	}
-	else{
-		document.getElementById("navbar-top").className += " navbar-default";
-		TweenMax.to(".navbar-default", 0.5, {opacity: 1, delay: 0.1});
-	}
-	
+	}	
 }
