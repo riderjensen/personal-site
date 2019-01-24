aee5a374a2ed7ac2d8cf5fe4f1ca6593e13a55fe
Rider <riderjensen@gmail.com>
2019-01-24 12:16:07 -0700

    Remove comments

--- app.js
+++ app.js
@@ -70,40 +70,40 @@ mongoose.connect('mongodb://localhost:27017/redditDays', {
 
 // Reddit bot
 
-// const r = new Snoowrap(redditUserInformation);
+const r = new Snoowrap(redditUserInformation);
 
-// const client = new Snoostorm(r);
+const client = new Snoostorm(r);
 
-// const streamOpts = {
-// 	subreddit: `funny+AskReddit+worldnews+pics+science+todayilearned+gaming+videos+movies+aww+IAmA+Music+gifs+news+blog+EarthPorn+askscience+explainlikeimfive+books+Showerthoughts+mildlyinteresting+television+LifeProTips+Jokes+DIY+space+food+gadgets+nottheonion+sports+Art+photoshopbattles+GetMotivated+tifu+Documentaries+UpliftingNews+listentothis+dataisbeautiful+history+Futurology+personalfinance+OldSchoolCool+philosophy+WritingPrompts+nosleep+creepy+TwoXChromosomes+technology+Fitness+AdviceAnimals+WTF+bestof+politics+interestingasfuck+wholesomememes+BlackPeopleTwitter+oddlysatisfying+memes+atheism+woahdude+leagueoflegends+facepalm+reactiongifs+relationships+europe+pcmasterrace+me_irl+travel+lifehacks+gonewild+Whatcouldgowrong+Unexpected+NatureIsFuckingLit+Overwatch+gameofthrones+dankmemes+nba+malefashionadvice+tattoos+Android+trippinthroughtime+Tinder+Games+programming+AnimalsBeingJerks+AnimalsBeingBros+PS4+instant_regret+mildlyinfuriating+FoodPorn+hiphopheads+CrappyDesign+dadjokes+GifRecipes+BikiniBottomTwitter+soccer+trees+buildapc+pokemon+HistoryPorn+nonononoyes+loseit+pokemongo+Damnthatsinteresting+HighQualityGifs+gardening+humor+boardgames+Eyebleach+OutOfTheLoop+nsfw+sex+NSFW_GIF+BetterEveryLoop+RealGirls+rarepuppers+comics+ContagiousLaughter+wheredidthesodago+Wellthatsucks+xboxone+BeAmazed+EatCheapAndHealthy+Cooking+trashy+Bitcoin+rickandmorty+AnimalsBeingDerps+ChildrenFallingOver+cringepics+relationship_advice+Frugal+cats+itookapicture+pcgaming+hmmm+4chan+StarWars+FortNiteBR+iphone+RoastMe+cars+photography+nfl+NintendoSwitch+MadeMeSmile+YouShouldKnow+hearthstone+quityourbullshit+teenagers+scifi+wow+keto+apple+confession+recipes+HumansBeingBros+youtubehaiku+cringe+blackmagicfuckery+AskHistorians+bodyweightfitness+slowcooking+FiftyFifty+MealPrepSunday+MovieDetails+WhitePeopleTwitter+holdmybeer+reallifedoodles+streetwear+woodworking+NetflixBestOf+AskMen+ImGoingToHellForThis+PeopleFuckingDying+Minecraft+iamverysmart+baseball+MakeupAddiction+nevertellmetheodds+anime+RoomPorn+howto+insanepeoplefacebook+DunderMifflin+Roadcam+learnprogramming+battlestations+whatisthisthing+PrequelMemes+SkincareAddiction+DestinyTheGame+offmychest+thatHappened+PerfectTiming+AskWomen+PUBATTLEGROUNDS+DnD+nintendo+CryptoCurrency+dogs+PewdiepieSubmissions+shittyaskscience+comicbooks+GlobalOffensive+MemeEconomy+NoStupidQuestions+holdthemoan+GamePhysics+therewasanattempt+Awwducational+TrollXChromosomes+conspiracy+frugalmalefashion+madlads+youseeingthisshit+entertainment+oldpeoplefacebook+educationalgifs+urbanexploration+WatchPeopleDieInside+MurderedByWords+ChoosingBeggars+backpacking+natureismetal+raspberry_pi+AbandonedPorn+nononono+hockey+ProgrammerHumor+wallpaper+The_Donald+femalefashionadvice+assholedesign+MMA+savedyouaclick+niceguys+ATBGE+socialskills+BustyPetite+starterpacks+changemyview+manga+drawing+shittyfoodporn+horror+TrollYChromosome+OopsDidntMeanTo+math+legaladvice`,
-// 	requestDelay: 5000
-// };
+const streamOpts = {
+	subreddit: `funny+AskReddit+worldnews+pics+science+todayilearned+gaming+videos+movies+aww+IAmA+Music+gifs+news+blog+EarthPorn+askscience+explainlikeimfive+books+Showerthoughts+mildlyinteresting+television+LifeProTips+Jokes+DIY+space+food+gadgets+nottheonion+sports+Art+photoshopbattles+GetMotivated+tifu+Documentaries+UpliftingNews+listentothis+dataisbeautiful+history+Futurology+personalfinance+OldSchoolCool+philosophy+WritingPrompts+nosleep+creepy+TwoXChromosomes+technology+Fitness+AdviceAnimals+WTF+bestof+politics+interestingasfuck+wholesomememes+BlackPeopleTwitter+oddlysatisfying+memes+atheism+woahdude+leagueoflegends+facepalm+reactiongifs+relationships+europe+pcmasterrace+me_irl+travel+lifehacks+gonewild+Whatcouldgowrong+Unexpected+NatureIsFuckingLit+Overwatch+gameofthrones+dankmemes+nba+malefashionadvice+tattoos+Android+trippinthroughtime+Tinder+Games+programming+AnimalsBeingJerks+AnimalsBeingBros+PS4+instant_regret+mildlyinfuriating+FoodPorn+hiphopheads+CrappyDesign+dadjokes+GifRecipes+BikiniBottomTwitter+soccer+trees+buildapc+pokemon+HistoryPorn+nonononoyes+loseit+pokemongo+Damnthatsinteresting+HighQualityGifs+gardening+humor+boardgames+Eyebleach+OutOfTheLoop+nsfw+sex+NSFW_GIF+BetterEveryLoop+RealGirls+rarepuppers+comics+ContagiousLaughter+wheredidthesodago+Wellthatsucks+xboxone+BeAmazed+EatCheapAndHealthy+Cooking+trashy+Bitcoin+rickandmorty+AnimalsBeingDerps+ChildrenFallingOver+cringepics+relationship_advice+Frugal+cats+itookapicture+pcgaming+hmmm+4chan+StarWars+FortNiteBR+iphone+RoastMe+cars+photography+nfl+NintendoSwitch+MadeMeSmile+YouShouldKnow+hearthstone+quityourbullshit+teenagers+scifi+wow+keto+apple+confession+recipes+HumansBeingBros+youtubehaiku+cringe+blackmagicfuckery+AskHistorians+bodyweightfitness+slowcooking+FiftyFifty+MealPrepSunday+MovieDetails+WhitePeopleTwitter+holdmybeer+reallifedoodles+streetwear+woodworking+NetflixBestOf+AskMen+ImGoingToHellForThis+PeopleFuckingDying+Minecraft+iamverysmart+baseball+MakeupAddiction+nevertellmetheodds+anime+RoomPorn+howto+insanepeoplefacebook+DunderMifflin+Roadcam+learnprogramming+battlestations+whatisthisthing+PrequelMemes+SkincareAddiction+DestinyTheGame+offmychest+thatHappened+PerfectTiming+AskWomen+PUBATTLEGROUNDS+DnD+nintendo+CryptoCurrency+dogs+PewdiepieSubmissions+shittyaskscience+comicbooks+GlobalOffensive+MemeEconomy+NoStupidQuestions+holdthemoan+GamePhysics+therewasanattempt+Awwducational+TrollXChromosomes+conspiracy+frugalmalefashion+madlads+youseeingthisshit+entertainment+oldpeoplefacebook+educationalgifs+urbanexploration+WatchPeopleDieInside+MurderedByWords+ChoosingBeggars+backpacking+natureismetal+raspberry_pi+AbandonedPorn+nononono+hockey+ProgrammerHumor+wallpaper+The_Donald+femalefashionadvice+assholedesign+MMA+savedyouaclick+niceguys+ATBGE+socialskills+BustyPetite+starterpacks+changemyview+manga+drawing+shittyfoodporn+horror+TrollYChromosome+OopsDidntMeanTo+math+legaladvice`,
+	requestDelay: 5000
+};
 
-// const comments = client.CommentStream(streamOpts);
+const comments = client.CommentStream(streamOpts);
 
-// let countingObject = {};
+let countingObject = {};
 
-// comments.on('comment', (comment) => {
-// 	let commentSub = comment.subreddit_name_prefixed;
-// 	if (countingObject[commentSub]) {
-// 		countingObject[commentSub].com++;
-// 	} else {
-// 		countingObject[commentSub] = {
-// 			com: 1,
-// 			found: 0
-// 		}
-// 	}
-// 	if (comment.body.includes(wordSearchFor)) {
-// 		countingObject[commentSub].found++;
-// 	}
-// });
+comments.on('comment', (comment) => {
+	let commentSub = comment.subreddit_name_prefixed;
+	if (countingObject[commentSub]) {
+		countingObject[commentSub].com++;
+	} else {
+		countingObject[commentSub] = {
+			com: 1,
+			found: 0
+		}
+	}
+	if (comment.body.includes(wordSearchFor)) {
+		countingObject[commentSub].found++;
+	}
+});
 
-// setInterval(() => {
-// 	const newDayModel = new DayModel({
-// 		items: countingObject
-// 	});
-// 	newDayModel.save().then(response => {
-// 		countingObject = {};
-// 	})
-// 	console.log(countingObject)
-// }, 360000)
+setInterval(() => {
+	const newDayModel = new DayModel({
+		items: countingObject
+	});
+	newDayModel.save().then(response => {
+		countingObject = {};
+	})
+	console.log(countingObject)
+}, 360000)
