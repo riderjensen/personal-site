0533d84bbc1beb4d89d6bc56050620aa3638f936
Rider <riderjensen@gmail.com>
2019-01-24 14:28:07 -0700

    Fixing the find method

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,13 +1,13 @@
 const DaySave = require('../models/daySave');
 
 exports.getAll = (req, res, next) => {
-	DaySave.find({}, (items) => {
-		console.log(items);
-		if (items == null) {
-			res.status(500).send('The db is null for some reason');
-		} else {
-			res.status(200).send(items);
-		}
-	});
-
+	newDay.save().then(result => {
+		DaySave.find().then(items => {
+			if (items == null) {
+				res.status(500).send('The db is null for some reason');
+			} else {
+				res.status(200).send(items);
+			}
+		});
+	})
 }
