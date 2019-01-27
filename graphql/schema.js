97de30231c4f430f142b842103b414aed1094c8b
Rider <riderjensen@gmail.com>
2019-01-27 11:19:38 -0700

    Change schema around

--- graphql/schema.js
+++ graphql/schema.js
@@ -2,15 +2,16 @@ const {
 	buildSchema
 } = require('graphql');
 
-module.exports = buildSchema(`
-	type RootMutation {
+module.exports = buildSchema(`	
+	type Test {
+		title: String
+	}
 
-	}
 	type RootQuery {
-		
+		boop: Test
 	}
+
 	schema {
 		query: RootQuery
-		mutation: RootMutation
 	}
 `);
