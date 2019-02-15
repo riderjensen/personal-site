24879efbd62337f6452e46f2377c914cd6ba8978
Rider <riderjensen@gmail.com>
2019-02-15 14:24:43 -0700

    Add an extra link for my sites

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,5 @@
-/*jslint browser:true */
 "use strict()";
 
-
-window.onscroll = function() {scrollFunction();};
-function scrollFunction() {
-	if(window.innerWidth >= 768){
-	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-			TweenMax.to(".navbar-default", 0.5, {opacity: 1, delay: 0.1});
-    }
-     else {
-			TweenMax.to(".navbar-default", 0.5, {opacity: 0, delay: 0.1});
-			
-		}
-	}	
-}
-
-$('.navbar-collapse a').click(function(){
-    $(".navbar-collapse").collapse('hide');
+$('.navbar-collapse a').click(function () {
+	$(".navbar-collapse").collapse('hide');
 });
