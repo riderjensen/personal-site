c0ec87a8dd264573dba1adb8b234ff3c02ed94f1
Unknown <riderjensen@gmail.com>
2019-03-06 13:02:22 -0700

    Add new items to schema

--- graphql/schema.js
+++ graphql/schema.js
@@ -4,6 +4,18 @@ const {
 
 module.exports = buildSchema(`	
 
+
+	type subName {
+		com: Int!
+		found: Int!
+	}
+
+	input comFound{
+		com: Int!
+		found: Int = 0
+	}
+	
+
 	type getObj {
 		id: String!
 		createdAt: String!
@@ -11,10 +23,22 @@ module.exports = buildSchema(`
 	}
 
 	type RootQuery {
+		getOneById(id: ID!): getObj 
 		getNames: [getObj]
+		getSubName(name: String!): subName
+		getRange(sub: String!, start: String!, end: String): [subName]!
+		getCopmbinedRange(sub: String!, start: String!, end: String): subName!
+	}
+
+	type RootMutation {
+		deleteDataPoint(id: ID!): Boolean
+		deleteSubDataPoint(id: ID!, sub: String!): Boolean
+		createNewItem: getObj
+		editItem(id: ID!, sub: String!, completedFound: comFound!): Boolean
 	}
 
 	schema {
 		query: RootQuery
+		mutation: RootMutation
 	}
 `);
