5cdbd8990fe4bd9b01bd5275e63c4d60b0bb5e45
Rider Jensen <riderjensen@gmail.com>
2019-03-12 10:38:09 -0600

    add new resolvers got combines

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,42 @@
 const DaySave = require('../models/daySave');
 
 module.exports = {
-	getNames: async () => {
-		const resp = await DaySave.find();
-		return resp.map(item => item)
+	getAllItems: async () => {
+		const resp = await DaySave.find()
+
+		const newArray = [];
+
+		for (let i = 0; i < resp.length; i++) {
+			if (resp[i].items != undefined) {
+				const obj = {
+					id: resp[i]._id,
+					updatedAt: resp[i].updatedAt,
+					createdAt: resp[i].createdAt,
+					items: {}
+				}
+				for (let item in resp[i].items) {
+					obj.items[item.split('/')[1]] = {
+						...resp[i].items[item]
+					}
+				}
+				newArray.push(obj)
+			}
+		}
+		return newArray;
 	},
 	getOneById: async (args) => {
-		const resp = await DaySave.findById(args.id);
+		const resp = await DaySave.findById(args.id)
 		if (!resp) throw new error('We cant find an item in the DB with this ID');
-		resp.item = resp.items[`r/${args.sub}`];
-		return resp;
+		let newObj = {
+			id: resp._id,
+			createdAt: resp.createdAt,
+			updatedAt: resp.updatedAt,
+			items: {}
+		};
+		for (let item in resp.items) {
+			newObj.items[item.split('/')[1]] = resp.items[item]
+		}
+		return newObj;
 	},
 	getSubName: async (args) => {
 		const subName = `r/${args.name}`;
@@ -27,8 +54,6 @@ module.exports = {
 		return newObj
 	},
 	getRange: async (args) => {
-		const theSub = `r/${args.sub}`;
-
 		const resp = await DaySave.find({
 			"createdAt": {
 				"$gte": new Date(parseInt(args.start)),
@@ -39,14 +64,16 @@ module.exports = {
 		const newArray = [];
 
 		for (let i = 0; i < resp.length; i++) {
-			if (resp[i].items != undefined && resp[i].items[theSub] != undefined) {
+			if (resp[i].items != undefined) {
 				const obj = {
 					id: resp[i]._id,
 					updatedAt: resp[i].updatedAt,
 					createdAt: resp[i].createdAt,
-					item: {
-						com: resp[i].items[theSub].com,
-						found: resp[i].items[theSub].found
+					items: {}
+				}
+				for (let item in resp[i].items) {
+					obj.items[item.split('/')[1]] = {
+						...resp[i].items[item]
 					}
 				}
 				newArray.push(obj)
@@ -55,26 +82,32 @@ module.exports = {
 		return newArray;
 	},
 	getCombinedRange: async (args) => {
-		const theSub = `r/${args.sub}`;
-
 		const resp = await DaySave.find({
 			"createdAt": {
 				"$gte": new Date(parseInt(args.start)),
-				"$lt": args.end != undefined ? new Date(parseInt(args.end)) : new Date(2020, 1, 10)
+				"$lt": args.end != undefined ? new Date(parseInt(args.end) + 1) : new Date(2020, 1, 10)
 			}
 		})
+		const obj = {
+			items: {
 
-		let newObject = {
-			com: 0,
-			found: 0
-		};
-		for (let i = 0; i < resp.length; i++) {
-			if (resp[i].items != undefined && resp[i].items[theSub] != undefined) {
-				newObject.com += resp[i].items[theSub].com
-				newObject.found += resp[i].items[theSub].found
 			}
 		}
-		return newObject
+
+		for (let i = 0; i < resp.length; i++) {
+			if (resp[i].items != undefined) {
+				for (let item in resp[i].items) {
+					obj.items[item.split('/')[1]] ? null : obj.items[item.split('/')[1]] = {
+						com: 0,
+						found: 0
+					};
+					obj.items[item.split('/')[1]].com += resp[i].items[item].com === null ? 0 : resp[i].items[item].com
+					obj.items[item.split('/')[1]].found += resp[i].items[item].found === null ? 0 : resp[i].items[item].found
+				}
+			}
+		}
+		return obj;
+
 	},
 	deleteDataPoint: async (args) => {
 		const resp = await DaySave.findByIdAndDelete(args.id);
