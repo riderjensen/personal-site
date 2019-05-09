b86a1793042fed31545111a6c518ea0e8fe4335c
Rider Jensen <riderjensen@gmail.com>
2019-05-09 08:57:11 -0600

    Remove application DB connection over securty

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const helmet = require('helmet');
 
-const graphqlHTTP = require('express-graphql');
-const graphqlSchema = require('./graphql/schema');
-const graphqlResolver = require('./graphql/resolvers');
-
-const redditUserInformation = require('../env');
-
-const apiRoute = require('./routes/api.route');
 const offbrandRoute = require('./routes/offbrand.route');
 
 
@@ -27,7 +19,7 @@ app.use(helmet());
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Methods', 'GET');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 	if (req.method === 'OPTIONS') {
 		return res.sendStatus(200);
@@ -38,36 +30,14 @@ app.use((req, res, next) => {
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-app.use('/api', apiRoute);
+app.get('/api', (req, res) => res.send('<p>This route has been closed due to security concerns. You are welcome to view the source code on <a href="https://github.com/riderjensen/Personal-Site">github</a></p>'));
+
+app.get('/sub', (req, res) => res.send('<p>This route has been closed due to security concerns. You are welcome to view the source code on <a href="https://github.com/riderjensen/Personal-Site">github</a></p>'));
 
 app.use('/offbrand', offbrandRoute);
-
-app.use('/sub', graphqlHTTP({
-	schema: graphqlSchema,
-	rootValue: graphqlResolver,
-	graphiql: true,
-	formatError(err) {
-		if (!err.originalError) {
-			return err;
-		}
-		const data = err.originalError.data;
-		const message = err.message || 'An error occured';
-		const code = err.originalError.code || 500;
-		return {
-			message: message,
-			status: code,
-			data,
-			data
-		};
-	}
-}));
 
 app.get('/', (req, res) => res.render('index'));
 
 app.get('*', (req, res) => res.redirect('/'))
 
-mongoose.connect(`mongodb+srv://${redditUserInformation.username}:${redditUserInformation.password}@nodecourse-zfafv.mongodb.net/redditDays?retryWrites=true`, {
-	useNewUrlParser: true
-}).then(() => {
-	app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-}).catch(err => console.log(err))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
